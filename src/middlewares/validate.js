73b88cdec6e867505cd1f7f062f1daa3b02d6aef
clunckyboy <edwinjonatanpurba@gmail.com>
2026-07-21 09:29:39 +0700

    feat: adding exceptions, middlewares, security, utils, and server setup

---
+++ src/middlewares/validate.js
@@ -0,0 +1,12 @@
+export const validate = (schema) => (req, res, next) => {
+  const { error, value } = schema.validate(req.body, {
+    abortEarly: false,
+    allowUnknown: false,
+    stripUnknown: true
+  });
+
+  if (error) return next(error);
+
+  req.validated = value;
+  next();
+};
