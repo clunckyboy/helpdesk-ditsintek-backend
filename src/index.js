6b5915307bd4f871d194ea724a4e19f4b1d26145
clunckyboy <edwinjonatanpurba@gmail.com>
2026-07-23 12:05:09 +0700

    fix: minor fix at script start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import server from './server/index.js';
 
-if (process.env.NODE_ENV !== 'production') {
+if (!process.env.VERCEL) {
   const port = process.env.PORT || 3000;
   const host = process.env.HOST;
   
-  server.listen(port, () => {
+  server.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://${host}:${port}`);
-  })
+  });
 }
 
 export default server;
