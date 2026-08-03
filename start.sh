c720f50dcd765934da7b061d9a4a9c0032d44162
clunckyboy <edwinjonatanpurba@gmail.com>
2026-08-03 16:24:17 +0700

    debug

--- start.sh
+++ start.sh
@@ -11,10 +11,14 @@ chmod 600 ~/.ssh/id_rsa
 echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
 # 4. Menyalakan autossh di background
-autossh -M 0 -N -L 5432:localhost:5432 $SSH_USER@$SSH_HOST &
+autossh -M 0 -N -L 5432:127.0.0.1:5432 $SSH_USER@$SSH_HOST > tunnel.log 2>&1 &
 
 # 5. Memberi jeda 3 detik agar tunnel sempat terbuka
 sleep 3
+
+echo "=== HASIL LOG SSH TUNNEL ==="
+cat tunnel.log
+echo "============================"
 
 # 6. Menyalakan aplikasi Node.js
 npm run start
