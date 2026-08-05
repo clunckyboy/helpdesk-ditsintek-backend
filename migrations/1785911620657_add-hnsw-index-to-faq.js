4b8f6e6dbb8a66aed0fb389112640edbf70af1c1
clunckyboy <edwinjonatanpurba@gmail.com>
2026-08-05 13:39:51 +0700

    feat: adding hnsw index on database

---
+++ migrations/1785911620657_add-hnsw-index-to-faq.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.sql(`
+    CREATE INDEX idx_faq_embeddings
+    ON faq
+    USING hnsw (embeddings vector_cosine_ops)
+    WITH (m = 16, ef_construction = 64);  
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.sql(`DROP INDEX IF EXISTS idx_faq_embeddings`);
+};
